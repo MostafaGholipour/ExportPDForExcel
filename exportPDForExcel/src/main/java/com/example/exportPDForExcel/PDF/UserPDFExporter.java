ba1b4de098f73ac81c387f68adb657b3b94c679d
admin <Mostafagholipore@gmail.com>
2024-07-20 10:36:11 +0330

    General item

---
+++ exportPDForExcel/src/main/java/com/example/exportPDForExcel/PDF/UserPDFExporter.java
@@ -0,0 +1,83 @@
+package com.example.exportPDForExcel.PDF;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.exportPDForExcel.model.User;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class UserPDFExporter<T> {
+    private final Class<T> tClass;
+    private final List<T> listUsers;
+    private final List<Field> fields;
+
+    public UserPDFExporter(Class<T> tClass, List<T> listUsers) {
+        this.tClass = tClass;
+        this.listUsers = listUsers;
+        Field[] fieldsArray = tClass.getDeclaredFields();
+        fields = new ArrayList<>(Arrays.asList(fieldsArray));
+    }
+
+    public UserPDFExporter(Class<T> tClass, List<T> listUsers, List<String> fieldNames) {
+        this.tClass = tClass;
+        this.listUsers = listUsers;
+        Field[] fieldsArray = tClass.getDeclaredFields();
+        List<Field> list = new ArrayList<>();
+        for (String s : fieldNames) {
+            for (Field field : fieldsArray) {
+                if (field.getName().equalsIgnoreCase(s)) {
+                    list.add(field);
+                }
+            }
+        }
+        fields = list;
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+//        Field[] fields = tClass.getDeclaredFields();
+        for (Field field : fields) {
+            PdfPCell header = new PdfPCell();
+            header.setPhrase(new Phrase(field.getName()));
+            header.setBackgroundColor(Color.gray);
+            header.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(header);
+        }
+    }
+
+    private void writeTableData(PdfPTable table) {
+        for (T user : listUsers) {
+//            Field[] fields = tClass.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                try {
+                    Object value = field.get(user);
+                    table.addCell(value != null ? value.toString() : "");
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+//        PdfPTable table = new PdfPTable(tClass.getDeclaredFields().length);
+        PdfPTable table = new PdfPTable(fields.size());
+        table.setWidthPercentage(100);
+        writeTableHeader(table);
+        writeTableData(table);
+
+        document.add(table);
+        document.close();
+    }
+}
--- exportPDForExcel/src/main/java/com/example/exportPDForExcel/controller/UserController.java
+++ exportPDForExcel/src/main/java/com/example/exportPDForExcel/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.exportPDForExcel.controller;
 
+import com.example.exportPDForExcel.PDF.UserPDFExporter;
 import com.example.exportPDForExcel.model.User;
 import com.example.exportPDForExcel.Excel.UserExcelExporter;
 import com.example.exportPDForExcel.service.UserServices;
+import com.lowagie.text.DocumentException;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -33,9 +36,32 @@ public class UserController {
 
         List<User> listUsers = service.listAll();
 
-        UserExcelExporter excelExporter = new UserExcelExporter(User.class,listUsers);
+        UserExcelExporter excelExporter = new UserExcelExporter(User.class, listUsers);
 
         excelExporter.export(response);
+    }
+
+    @GetMapping("/users/export/pdf")
+    public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("+yyyy.MM.dd - HH.mm.ss+");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<User> listUsers = service.listAll();
+
+
+        List<String> fields = new ArrayList<>();
+        fields.add("iD");
+        fields.add("email");
+        fields.add("fullName");
+
+        UserPDFExporter exporter = new UserPDFExporter(User.class, listUsers,fields);
+        exporter.export(response);
+
     }
 
 }
